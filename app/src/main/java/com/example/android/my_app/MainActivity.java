3fdb4741d31aba9f7bb90323d6d658ef6f6e0984
Pranjali Maru <pranjalimaru@gmail.com>
2019-03-20 00:13:17 +0530

    Activities

--- app/src/main/java/com/example/android/my_app/MainActivity.java
+++ app/src/main/java/com/example/android/my_app/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.android.my_app;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void tutorial(View view){
+        Intent tut = new Intent(MainActivity.this, Tutorial.class);
+        startActivity(tut);
+    }
+
+    public void alarm(View view){
+        Intent alm = new Intent(MainActivity.this, Alarm.class);
+        startActivity(alm);
+    }
+
+    public void notepad(View view){
+        Intent ntp = new Intent(MainActivity.this, Notepad.class);
+        startActivity(ntp);
+    }
+
+    public void summary(View view){
+        Intent sum = new Intent(MainActivity.this, Summary.class);
+        startActivity(sum);
     }
 }
---
+++ app/src/main/java/com/example/android/my_app/Tutorial.java
@@ -0,0 +1,15 @@
+package com.example.android.my_app;
+
+import android.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toolbar;
+
+public class Tutorial extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tutorial);
+    }
+    }
