69e161410ebf4303f4e2625beb56d6fc2442d1d1
Pranjali Maru <pranjalimaru@gmail.com>
2019-03-21 20:31:29 +0530

    Activities

--- app/src/main/java/com/example/android/my_app/Tutorial.java
+++ app/src/main/java/com/example/android/my_app/Tutorial.java
@@ -1,9 +1,11 @@
 package com.example.android.my_app;
 
-import android.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Toolbar;
+import android.speech.tts.TextToSpeech;
+import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebView;
+import android.widget.Button;
+
 
 public class Tutorial extends AppCompatActivity {
 
@@ -11,5 +13,8 @@ public class Tutorial extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tutorial);
+
+        WebView webView = (WebView)findViewById(R.id.youtubeview);
+        webView.loadUrl("https://www.youtube.com/watch?v=Raa0vBXA8OQ");
     }
-    }
+}
