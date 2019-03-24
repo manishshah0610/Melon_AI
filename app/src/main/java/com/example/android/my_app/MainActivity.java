76c83ba7c62d1395f7a8ca554afd5541eee91ae8
mansi59054 <mansiod2049@gmail.com>
2019-03-24 12:42:57 +0530

    Icons

--- app/src/main/java/com/example/android/my_app/MainActivity.java
+++ app/src/main/java/com/example/android/my_app/MainActivity.java
@@ -4,10 +4,11 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Toolbar;
+import android.widget.RelativeLayout;
 
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
