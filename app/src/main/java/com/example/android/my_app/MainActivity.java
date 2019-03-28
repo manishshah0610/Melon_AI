ccaaad7715d905b9987e913334877ad3f041ecb6
mansi59054 <mansiod2049@gmail.com>
2019-03-29 00:09:35 +0530

    New

--- app/src/main/java/com/example/android/my_app/MainActivity.java
+++ app/src/main/java/com/example/android/my_app/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.RelativeLayout;
+import android.widget.Switch;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -14,6 +15,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // initiate a Switch
+        Switch simpleSwitch = (Switch) findViewById(R.id.simpleSwitch);
+
+// check current state of a Switch (true or false).
+        Boolean switchState = simpleSwitch.isChecked();
     }
 
     public void tutorial(View view){
@@ -34,5 +41,10 @@ public class MainActivity extends AppCompatActivity {
     public void summary(View view){
         Intent sum = new Intent(MainActivity.this, Summary.class);
         startActivity(sum);
+    }
+
+    public void setting(View view){
+        Intent set = new Intent(MainActivity.this, Settings.class);
+        startActivity(set);
     }
 }
---
+++ app/src/main/java/com/example/android/my_app/splash.java
@@ -0,0 +1,36 @@
+
+package com.example.android.my_app;
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class splash extends AppCompatActivity {
+    private static int SPLASH_TIME_OUT =4000;
+    private ImageView logo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        logo= findViewById(R.id.logo);
+
+        Animation maim = AnimationUtils.loadAnimation(this,R.anim.mysplashanimation);
+        logo.startAnimation(maim);
+
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run(){
+                Intent homeIntent = new Intent(splash.this,MainActivity.class);
+                //to run the next activity
+                startActivity(homeIntent);
+                finish();
+            }
+        },SPLASH_TIME_OUT);
+    }
+    }
+
