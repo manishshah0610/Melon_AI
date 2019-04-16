f7296616a6c26ae18be2f89adea9c8fbb1167f29
mansi59054 <mansiod2049@gmail.com>
2019-04-16 21:27:59 +0530

    update till tuesday

--- app/src/main/java/com/example/android/my_app/login.java
+++ app/src/main/java/com/example/android/my_app/login.java
@@ -1,7 +1,12 @@
 package com.example.android.my_app;
 
+import android.app.AlertDialog;
 import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -31,37 +36,40 @@ public class login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        Name = (EditText)findViewById(R.id.etName);
-        Password = (EditText)findViewById(R.id.etPassword);
-        Info = (TextView)findViewById(R.id.tvInfo);
-        Login = (Button)findViewById(R.id.btnLogin);
-        userRegistration= (TextView) findViewById(R.id.tvRegister);
+        Name = (EditText) findViewById(R.id.etName);
+        Password = (EditText) findViewById(R.id.etPassword);
+        Info = (TextView) findViewById(R.id.tvInfo);
+        Login = (Button) findViewById(R.id.btnLogin);
+        userRegistration = (TextView) findViewById(R.id.tvRegister);
         Info.setText("No of attempts remaining: 5");
         firebaseAuth = FirebaseAuth.getInstance();
         progressDialog = new ProgressDialog(this);
         FirebaseUser user = firebaseAuth.getCurrentUser();
         forgotPassword = (TextView) findViewById(R.id.tvForgotPassword);
-        if(user!=null)
-        {
+        if (user != null) {
             finish();
-            startActivity(new Intent(login.this,MainActivity.class));
+            startActivity(new Intent(login.this, MainActivity.class));
         }
         Login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                validate(Name.getText().toString(), Password.getText().toString());
+                if (Name.getText().toString().isEmpty() || Password.getText().toString().isEmpty()) {
+                    Toast.makeText(login.this, "Please enter all details to login", Toast.LENGTH_SHORT).show();
+                } else {
+                    validate(Name.getText().toString(), Password.getText().toString());
+                }
             }
         });
         userRegistration.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(login.this,register.class));
+                startActivity(new Intent(login.this, register.class));
             }
         });
         forgotPassword.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(login.this,password.class));
+                startActivity(new Intent(login.this, password.class));
             }
         });
     }
@@ -105,4 +113,9 @@ public class login extends AppCompatActivity {
         }
 
     }
+    @Override
+    public void onBackPressed() {
+        finishAffinity();
+    }
+
 }
